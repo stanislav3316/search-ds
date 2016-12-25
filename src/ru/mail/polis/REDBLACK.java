e3f069f5a41bebdc9cb40d76365078e67966e183
stanislav3316 <stanislav3316@gmail.com>
2016-12-25 22:39:48 +0300

    update RB tree

---
+++ src/ru/mail/polis/REDBLACK.java
@@ -0,0 +1,7 @@
+package ru.mail.polis;
+
+/**
+ * Created by iters on 12/25/16.
+ */
+public class REDBLACK {
+}
--- src/ru/mail/polis/RedBlackTree.java
+++ src/ru/mail/polis/RedBlackTree.java
@@ -3,37 +3,36 @@ package ru.mail.polis;
 import java.util.*;
 
 //TODO: write code here
-public class RedBlackTree<E extends Comparable<E>> implements ISortedSet<E> {
-    private static final boolean RED = true;
-    private static final boolean BLACK = false;
-    private Node root;
-    private int size;
-    private final Comparator<E> comparator;
+public class RedBlackTree<T extends Comparable<T>> implements ISortedSet<T> {
+    enum Color {RED, BLACK}
+    private Node root = null;
+    private Node NIL = new Node(null, null, null, null, Color.BLACK);
+    private int size = 0;
+    private final Comparator<T> comparator;
 
     private class Node {
-        E value;
-        int N;
-        Node left, right;
-        boolean color;
+        Color color;
+        Node left, right, parent;
+        T data;
 
-        Node(E value, int N, boolean color) {
-            this.value = value;
-            this.N = N;
-            this.color = color;
+        Node(T data, Node left, Node right, Node parent, Color color) {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+            this.parent = parent;
+            this.color = Color.BLACK;
         }
 
         @Override
-        public String toString() {
-            final StringBuilder sb = new StringBuilder("N{");
-            sb.append("d=").append(value);
-            if (left != null) {
-                sb.append(", l=").append(left);
+        public boolean equals(Object o) {
+            if (!Node.class.isInstance(o)) {
+                return false;
             }
-            if (right != null) {
-                sb.append(", r=").append(right);
-            }
-            sb.append('}');
-            return sb.toString();
+
+            Node temp = (Node) o;
+            return data == temp.data && left == temp.left &&
+                    right == temp.right && parent == temp.parent &&
+                    color == temp.color;
         }
     }
 
@@ -41,12 +40,12 @@ public class RedBlackTree<E extends Comparable<E>> implements ISortedSet<E> {
         this.comparator = null;
     }
 
-    public RedBlackTree(Comparator<E> comparator) {
+    public RedBlackTree(Comparator<T> comparator) {
         this.comparator = comparator;
     }
 
     @Override
-    public E first() {
+    public T first() {
         if (root == null) {
             return null;
         }
@@ -57,16 +56,16 @@ public class RedBlackTree<E extends Comparable<E>> implements ISortedSet<E> {
         }
 
         if (root.left == null && root.right != null) {
-            return root.right.value;
+            return root.right.data;
         } else if (root.left == null && root.right == null) {
-            return root.value;
+            return root.data;
         }
 
-        return curr.value;
+        return curr.data;
     }
 
     @Override
-    public E last() {
+    public T last() {
         if (root == null) {
             return null;
         }
@@ -77,28 +76,35 @@ public class RedBlackTree<E extends Comparable<E>> implements ISortedSet<E> {
         }
 
         if (root.right == null && root.left != null) {
-            return root.left.value;
+            return root.left.data;
         } else if (root.right == null && root.left == null) {
-            return root.value;
+            return root.data;
         }
 
-        return curr.value;
+        return curr.data;
     }
 
     @Override
-    public List<E> inorderTraverse() {
-        List<E> list = new ArrayList<E>(size);
+    public List<T> inorderTraverse() {
+        List<T> list = new ArrayList<T>(size);
         inorderTraverse(root, list);
         return list;
     }
 
-    private void inorderTraverse(Node curr, List<E> list) {
+    private void inorderTraverse(Node curr, List<T> list) {
         if (curr == null) {
             return;
         }
-        inorderTraverse(curr.left, list);
-        list.add(curr.value);
-        inorderTraverse(curr.right, list);
+
+        if (curr.left != null && !curr.left.equals(NIL)) {
+            inorderTraverse(curr.left, list);
+        }
+
+        list.add(curr.data);
+
+        if (curr.right != null && !curr.right.equals(NIL)) {
+            inorderTraverse(curr.right, list);
+        }
     }
 
     @Override
@@ -112,15 +118,15 @@ public class RedBlackTree<E extends Comparable<E>> implements ISortedSet<E> {
     }
 
     @Override
-    public boolean contains(E value) {
+    public boolean contains(T value) {
         Node curr = root;
 
         if (value == null) {
             throw new NullPointerException("Null Value");
         }
 
-        while (curr != null && compare(curr.value, value) != 0) {
-            if (compare(curr.value, value) < 0) {
+        while (curr != null && compare(curr.data, value) != 0) {
+            if (compare(curr.data, value) < 0) {
                 curr = curr.right;
             } else {
                 curr = curr.left;
@@ -130,205 +136,321 @@ public class RedBlackTree<E extends Comparable<E>> implements ISortedSet<E> {
         return curr != null;
     }
 
-    private boolean isRed(Node x) {
-        if (x == null)
-            return false;
-        return x.color == RED;
-    }
-
-    private Node rotateLeft(Node h) {
-        Node x = h.right;
-        h.right = x.left;
-        x.left = h;
-        x.color = h.color;
-        h.color = RED;
-        x.N = h.N;
-        h.N = 1 + size(h.left) + size(h.right);
-        return x;
-    }
-
-    private Node rotateRight(Node h) {
-        Node x = h.left;
-        h.left = x.right;
-        x.right = h;
-        x.color = h.color;
-        h.color = RED;
-        x.N = h.N;
-        h.N = 1 + size(h.left) + size(h.right);
-        return x;
-    }
-
-    private int size(Node x) {
-        if (x == null) {
-            return 0;
-        }
-        return x.N;
-    }
-
-    private void flipColors(Node h) {
-        h.color = !h.color;
-        h.left.color = !h.left.color;
-        h.right.color = !h.right.color;
-    }
-
-    private Node moveRedLeft(Node h) {
-        flipColors(h);
-        if (isRed(h.right.left)) {
-            h.right = rotateRight(h.right);
-            h = rotateLeft(h);
-        }
-        return h;
-    }
-
-    private Node moveRedRight(Node h) {
-        flipColors(h);
-        if (!isRed(h.left.left)) {
-            h = rotateRight(h);
-        }
-        return h;
-    }
-
     @Override
-    public boolean add(E value) {
-        try {
-            root = put(root, value);
-            root.color = BLACK;
-            size++;
-        } catch (Exception e) {
-            return false;
+    public boolean add(T value) {
+        if (value == null) {
+            throw new NullPointerException();
         }
+
+        Node put = new Node(value, NIL,NIL, null, Color.BLACK);
+        if (root == null) {
+            root = put;
+        } else {
+            // contains
+            Node curr = root;
+            while (curr != null && !curr.equals(NIL)) {
+                int num = compare(curr.data, value);
+                if (num == 0) {
+                    break;
+                }
+
+                if (compare(curr.data, value) < 0) {
+                    curr = curr.right;
+                } else {
+                    curr = curr.left;
+                }
+            }
+
+            if (curr != null && !curr.equals(NIL) && compare(curr.data, value) == 0) {
+                return false;
+            }
+
+            RBInsert(put);
+        }
+
+        size++;
         return true;
     }
 
-    private Node put(Node h, E value) {
-        if (h == null) {
-            return new Node(value, 1, RED);
-        }
+    private void RBInsert(Node z) {
+        Node y = NIL;
+        Node x = root;
 
-        if (compare(value, h.value) < 0) {
-            h.left = put(h.left, value);
-        } else if (compare(value, h.value) > 0) {
-            h.right = put(h.right, value);
-        } else {
-            h.value = value;
-        }
-
-        if (isRed(h.right) && !isRed(h.left)) {
-            h = rotateLeft(h);
-        }
-
-        if (isRed(h.left) && isRed(h.left.left)) {
-            h = rotateRight(h);
-        }
-
-        if (isRed(h.left) && isRed(h.right)) {
-            flipColors(h);
-        }
-
-        h.N = size(h.left) + size(h.right) + 1;
-        return h;
-    }
-
-    @Override
-    public boolean remove(E value) {
-        try {
-            if (!isRed(root.left) && !isRed(root.right)) {
-                root.color = RED;
-            }
-            root = delete(root, value);
-            if (!isEmpty()) {
-                root.color = BLACK;
-            }
-            size--;
-        } catch (Exception e) {
-            return false;
-        }
-
-        return true;
-    }
-
-    private Node delete(Node h, E value) {
-        if (compare(value, h.value) < 0) {
-            if (!isRed(h.left) && !isRed(h.left.left)) {
-                h = moveRedLeft(h);
-            }
-            h.left = delete(h.left, value);
-        } else {
-            if (isRed(h.left)) {
-                h = rotateRight(h);
-            }
-
-            if (value == h.value && h.right == null) {
-                return null;
-            }
-
-            if (!isRed(h.right) && !isRed(h.right.left)) {
-                h = moveRedRight(h);
-            }
-
-            if (value == h.value) {
-                h.value = get(h.right, min(h.right).value);
-                h.right = deleteMin(h.right);
-            } else {
-                h.right = delete(h.right, value);
-            }
-        }
-        return balance(h);
-    }
-
-    private Node min(Node x) {
-        if (x.left == null) {
-            return x;
-        } else {
-            return min(x.left);
-        }
-    }
-
-    private Node deleteMin(Node h) {
-        if (h.left == null) {
-            return null;
-        }
-
-        if (!isRed(h.left) && !isRed(h.left.left)) {
-            h = moveRedLeft(h);
-        }
-
-        h.left = deleteMin(h.left);
-        return balance(h);
-    }
-
-    private Node balance(Node h) {
-        if (isRed(h.right)) {
-            h = rotateLeft(h);
-        }
-
-        if (isRed(h.left) && isRed(h.left.left)) {
-            h = rotateRight(h);
-        }
-
-        if (isRed(h.left) && isRed(h.right)) {
-            flipColors(h);
-        }
-
-        h.N = size(h.left) + size(h.right) + 1;
-        return h;
-    }
-
-    private E get(Node x, E value) {
-        while (x != null) {
-            if (compare(value, x.value) < 0) {
+        while (!x.equals(NIL)) {
+            y = x;
+            if (compare(z.data, x.data) < 0) {
                 x = x.left;
-            } else if (compare(value, x.value) > 0) {
-                x = x.right;
             } else {
-                return x.value;
+                x = x.right;
+            }
+        }
+
+        z.parent = y;
+        if (y.equals(NIL)) {
+            root = z;
+        } else if (compare(z.data, y.data) < 0) {
+            y.left = z;
+        } else {
+            y.right = z;
+        }
+
+        z.left = NIL;
+        z.right = NIL;
+        z.color = Color.RED;
+        RBInsertFix(z);
+    }
+
+    private void RBInsertFix(Node z) {
+        Node y;
+        while (z.parent != null && z.parent.color == Color.RED) {
+            if (z.parent.equals(z.parent.parent.left)) {
+                y = z.parent.parent.right;
+                if (y.color == Color.RED) {
+                    z.parent.color = Color.BLACK;
+                    y.color = Color.BLACK;
+                    z.parent.parent.color = Color.RED;
+                    z = z.parent.parent;
+                } else {
+                    if (z.equals(z.parent.right)) {
+                        z = z.parent;
+                        leftRotate(z);
+                    }
+
+                    z.parent.color = Color.BLACK;
+                    z.parent.parent.color = Color.RED;
+                    rightRotate(z.parent.parent);
+                }
+            } else {
+                y = z.parent.parent.left;
+                if(y.color == Color.RED) {
+                    z.parent.color = Color.BLACK;
+                    y.color = Color.BLACK;
+                    z.parent.parent.color = Color.RED;
+                    z = z.parent.parent;
+                } else {
+                    if (z.equals(z.parent.left)) {
+                        z = z.parent;
+                        rightRotate(z);
+                    }
+
+                    z.parent.color = Color.BLACK;
+                    z.parent.parent.color = Color.RED;
+                    leftRotate(z.parent.parent);
+                }
+            }
+        }
+        root.color = Color.BLACK;
+    }
+
+    private void leftRotate(Node x) {
+        Node y = x.right;
+        x.right = y.left;
+
+        if (!y.left.equals(NIL)) {
+            y.left.parent = x;
+        }
+
+        y.parent = x.parent;
+        if (x.parent == null) {
+            root = y;
+        } else if (x.equals(x.parent.left)) {
+            x.parent.left = y;
+        } else {
+            x.parent.right = y;
+        }
+
+        y.left = x;
+        x.parent = y;
+    }
+
+    private void rightRotate(Node y) {
+        Node x = y.left;
+        y.left = x.right;
+
+        if (!x.right.equals(NIL)) {
+            x.right.parent = y;
+        }
+
+        x.parent = y.parent;
+        if (y.parent == null) {
+            root = x;
+        } else if (y.equals(y.parent.left)) {
+            y.parent.left = x;
+        } else {
+            y.parent.right = x;
+        }
+
+        x.right = y;
+        y.parent = x;
+    }
+
+    private Node getElement(Node node, T key) {
+        if (node != null) {
+            if (compare(node.data, key) < 0) {
+                return getElement(node.right, key);
+            } else if (compare(node.data, key) > 0) {
+                return getElement(node.left, key);
+            } else {
+                return node;
             }
         }
 
         return null;
     }
 
-    private int compare(E v1, E v2) {
+    private void RBDelete(Node z) {
+        Node x,y;
+        if (z.left.equals(NIL) || z.right.equals(NIL)) {
+            y = z;
+        } else {
+            y = TreeSuccessor(z);
+        }
+
+        if (!y.left.equals(NIL)) {
+            x = y.left;
+        } else {
+            x = y.right;
+        }
+
+        x.parent = y.parent;
+        if (y.parent == null) {
+            root = x;
+        } else if (y.equals(y.parent.left)) {
+            y.parent.left = x;
+        } else {
+            y.parent.right = x;
+        }
+
+        if (!y.equals(z)) {
+            z.data = y.data;
+        }
+
+        if (y.color == Color.BLACK) {
+            RBDeleteFixUp(x);
+        }
+
+        //return y;
+    }
+
+    private void RBDeleteFixUp(Node x) {
+        Node w;
+        while(!x.equals(root) && x.color == Color.BLACK) {
+            if (x.equals(x.parent.left)) {
+                w = x.parent.right;
+                if (w.color == Color.RED) {
+                    w.color = Color.BLACK;
+                    x.parent.color = Color.RED;
+                    leftRotate(x.parent);
+                    w = x.parent.right;
+                }
+
+                if (w.left.color == Color.BLACK && w.right.color == Color.BLACK){
+                    w.color = Color.RED;
+                    x = x.parent;
+                } else {
+                    if (w.right.color == Color.BLACK) {
+                        w.left.color = Color.BLACK;
+                        w.color = Color.RED;
+                        rightRotate(w);
+                        w = x.parent.right;
+                    }
+
+                    w.color = x.parent.color;
+                    x.parent.color = Color.BLACK;
+                    w.right.color = Color.BLACK;
+                    leftRotate(x.parent);
+                    x = root;
+                }
+            } else {
+                w = x.parent.left;
+                if (w.color == Color.RED){
+                    w.color = Color.BLACK;
+                    x.parent.color = Color.RED;
+                    rightRotate(x.parent);
+                    w = x.parent.left;
+                }
+
+                if (w.right.color == Color.BLACK && w.left.color == Color.BLACK) {
+                    w.color = Color.RED;
+                    x = x.parent;
+                } else {
+                    if (w.left.color == Color.BLACK) {
+                        w.right.color = Color.BLACK;
+                        w.color = Color.RED;
+                        leftRotate(w);
+                        w = x.parent.left;
+                    }
+
+                    w.color = x.parent.color;
+                    x.parent.color = Color.BLACK;
+                    w.left.color = Color.BLACK;
+                    rightRotate(x.parent);
+                    x = root;
+                }
+            }
+        }
+
+        x.color = Color.BLACK;
+    }
+
+    private Node TreeSuccessor(Node x) {
+        Node y;
+        if (!x.right.equals(NIL)) {
+            return minimumm(x.right);
+        }
+
+        y = x.parent;
+        while(!y.equals(NIL) && x.equals(y.right)) {
+            x = y;
+            y = y.parent;
+        }
+
+        return y;
+    }
+
+    private Node minimumm(Node x) {
+        while (!x.left.equals(NIL)) {
+            x = x.left;
+        }
+
+        return x;
+    }
+
+    @Override
+    public boolean remove(T value) {
+        if(value == null) {
+            throw new NullPointerException();
+        }
+
+        Node curr = root;
+        while (curr != null && !curr.equals(NIL)) {
+            int num = compare(curr.data, value);
+            if (num == 0) {
+                break;
+            }
+
+            if (compare(curr.data, value) < 0) {
+                curr = curr.right;
+            } else {
+                curr = curr.left;
+            }
+        }
+
+        if (curr == null || curr.equals(NIL)) {
+            return false;
+        }
+
+        if(size == 1) {
+            root = null;
+        } else {
+            RBDelete(curr);
+        }
+
+        size--;
+        return true;
+    }
+
+    private int compare(T v1, T v2) {
         return comparator == null ? v1.compareTo(v2) : comparator.compare(v1, v2);
     }
 
@@ -340,6 +462,7 @@ public class RedBlackTree<E extends Comparable<E>> implements ISortedSet<E> {
     public static void main(String[] args) {
         RedBlackTree<Integer> tree = new RedBlackTree<>();
         tree.add(10);
+        System.out.println(tree.contains(10));
         tree.add(5);
         tree.add(15);
         System.out.println(tree.inorderTraverse());
@@ -363,6 +486,11 @@ public class RedBlackTree<E extends Comparable<E>> implements ISortedSet<E> {
             tree.add(rnd.nextInt(50));
         }
         System.out.println(tree.inorderTraverse());
+
+        for (int i : tree.inorderTraverse()) {
+            System.out.println(i);
+        }
+
         tree = new RedBlackTree<>((v1, v2) -> {
             // Even first
             final int c = Integer.compare(v1 % 2, v2 % 2);
